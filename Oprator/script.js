a1946b8806f1adbec3008b40b66ac4d3d09311f5
Pramod Mahto <Mahtop324@gmail.com>
2025-05-08 16:22:41 +0545

    completed oprators

--- Oprator/script.js
+++ Oprator/script.js
@@ -1,3 +1,68 @@
-console.log("operators")
+//Arithmetic operators
+// + , -, *,/ , % ,++ ,--  , **
+// console.log(10+20)
+// console.log(30-10)
+// console.log(2*5)
+// console.log(10/2)
+// console.log(6%2)
+// let x=5
+// // console.log(++x)
+// console.log(--x)
+// console.log(5**2)
 
-// - ,+,/
+// Assignment Operators
+// =,+=,-=,/=,*=,%=,**=
+
+// let x = 5;
+//  x += 5;                       //x = x + 5;
+//  x -= 5;                       //x = x - 5;
+// x *= 5;                      //x = x *5;
+// x /= 5;                      //x = x /5;
+// x %= 5;                      //x = x %5;
+// x **= 2;                     //x = x **2
+// console.log(x);
+
+// JavaScript Comparison Operators
+// == , === ,> ,<,>= ,<=  , != ,!==, ?(ternary operator)  false? "hello" : "bye"
+// let a = "5";
+// let b = "5";
+// console.log(a == b);
+// console.log(a === b);
+
+// let a = 10;
+// let b = 7;
+// console.log(b>a)
+// console.log(a<b)
+// console.log(a>=b)
+// console.log(a<=b)
+// console.log(a!=b)
+// console.log(a !== b);
+// console.log(a > b ? "hello " : "bye");
+
+// JavaScript Logical Operators
+// && , || , !
+// condition1  && condition2
+// true  && true >> true
+// true  && false >> false
+// false  && true >> false
+// false  && false >> false
+// condition1  ||  condition2
+// true  || true >> true
+// true  || false >> true
+// false  || true >> true
+// false  || false >> false
+
+// let a = 7;
+// let b = 5;
+
+// console.log(a == b && a > b);
+// console.log(a == b && a > b);
+// console.log(a > b && a > b);
+
+// let a = 7;
+// let b = 5;
+// console.log(a > b || a == b);
+
+// let a=false
+// console.log(!a)
+
